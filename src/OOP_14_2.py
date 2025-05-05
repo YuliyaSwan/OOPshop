f2697c924b77ec5d5b4b244ea702bf4d0dc6dbd3
Krivova Yuliya <charrly13swan@gmail.com>
2025-05-05 22:35:01 +0300

    update project 14.2

---
+++ src/OOP_14_2.py
@@ -0,0 +1,70 @@
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.__price = price
+        self.quantity = quantity
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        self.__price = new_price
+
+    @classmethod
+    def new_product(cls, data: dict):
+        return cls(
+            name=data.get("name"),
+            description=data.get("description"),
+            price=data.get("price"),
+            quantity=data.get("quantity"),
+        )
+
+    # @classmethod
+    # def new_product(cls, name, description, price, quantity):
+    #     return cls(name, description, price, quantity)
+
+
+class Category:
+    name: str
+    description: str
+    products: list
+    category_count: int = 0
+    product_count: int = 0
+
+    def __init__(self, name, description, products=None):
+        self.name = name
+        self.description = description
+        self.__products = products if products else []
+
+        Category.category_count += 1
+
+        Category.product_count += len(products) if products else 0
+
+    def add_product(self, product):
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count += 1
+        else:
+            raise TypeError("Можно добавлять только объекты класса Product")
+
+    @property
+    def products(self):
+        products_str = ""
+        for product in self.__products:
+            products_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products_str.strip()
+
+    @property
+    def products_in_list(self):
+        return self.__products
---
+++ tests/test_OOP_14_2.py
@@ -0,0 +1,126 @@
+import pytest
+
+from src.OOP_14_2 import Category, Product
+
+
+@pytest.fixture
+def product1():
+    return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+
+
+@pytest.fixture
+def product2():
+    return Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+
+
+@pytest.fixture
+def product3():
+    return Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+
+@pytest.fixture
+def category1(product1, product2, product3):
+    Category.category_count = 0
+    Category.product_count = 0
+    return Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
+
+
+# @pytest.fixture
+# def category1():
+#     return Category(
+#         "Смартфоны",
+#         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+#         [
+#             {
+#                 "name": "Samsung Galaxy C23 Ultra",
+#                 "description": "256GB, Серый цвет, 200MP камера",
+#                 "price": 180000.0,
+#                 "quantity": 5,
+#             },
+#             {"name": "Iphone 15", "description": "512GB, Gray space", "price": 210000.0, "quantity": 8},
+#             {"name": "Xiaomi Redmi Note 11", "description": "1024GB, Синий", "price": 31000.0, "quantity": 14},
+#         ],
+#     )
+
+
+def test_init(product1):
+    assert product1.name == "Samsung Galaxy S23 Ultra"
+    assert product1.description == "256GB, Серый цвет, 200MP камера"
+    assert product1.price == 180000.0
+    assert product1.quantity == 5
+
+
+def test_product_price_setter(product1):
+    product1.price = 800
+    assert product1.price == 800
+
+
+def test_product_price_setter_error(product1, capfd):
+    original_price = product1.price
+    product1.price = 0
+    out, _ = capfd.readouterr()
+    assert "Цена не должна быть нулевая или отрицательная" in out
+    assert product1.price == original_price
+
+    product1.price = -100
+    out, _ = capfd.readouterr()
+    assert "Цена не должна быть нулевая или отрицательная" in out
+    assert product1.price == original_price
+
+
+def test_product_new_product():
+    data = {
+        "name": "Samsung Galaxy S23 Ultra",
+        "description": "256GB, Серый цвет, 200MP камера",
+        "price": 180000.0,
+        "quantity": 5,
+    }
+    product = Product.new_product(data)
+    assert isinstance(product, Product)
+    assert product.name == "Samsung Galaxy S23 Ultra"
+    assert product.description == "256GB, Серый цвет, 200MP камера"
+    assert product.price == 180000.0
+    assert product.quantity == 5
+
+
+def test_init_cat(category1):
+    assert category1.name == "Смартфоны"
+    assert (
+        category1.description
+        == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    )
+    expected_output = (
+        "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
+        "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n"
+        "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт."
+    )
+    assert category1.products == expected_output
+    # assert category1.products_in_list == [
+    #     {
+    #         "name": "Samsung Galaxy C23 Ultra",
+    #         "description": "256GB, Серый цвет, 200MP камера",
+    #         "price": 180000.0,
+    #         "quantity": 5,
+    #     },
+    #     {"name": "Iphone 15", "description": "512GB, Gray space", "price": 210000.0, "quantity": 8},
+    #     {"name": "Xiaomi Redmi Note 11", "description": "1024GB, Синий", "price": 31000.0, "quantity": 14},
+    # ]
+    assert Category.category_count == 1
+    assert Category.product_count == 3
+
+
+def test_category_add_product(category1):
+    new_product = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+    category1.add_product(new_product)
+    assert '55" QLED 4K, 123000.0 руб. Остаток: 7 шт.' in category1.products
+    assert Category.product_count == 4
+
+
+def test_category_products_in_list(category1):
+    result = category1.products_in_list
+    assert isinstance(result, list)
+    assert all(isinstance(product, Product) for product in result)
